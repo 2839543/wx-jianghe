d27930d093efb5d49decb631b914433844eb7a47
haoxiaoqi <xiaoqi.hao@10020.net>
2015-12-28 14:49:38 +0800

    first commit

---
+++ src/changeFileName-JH-1.bat
@@ -0,0 +1,21 @@
+﻿@echo off     
+echo       *******File Change Name*******   
+echo ------------------------------------ 
+echo         Version 0.0.1      
+echo                           
+echo             Change 2015-12-28    
+echo ------------------------------------
+ 
+echo. 
+echo * please input new filename and filetype
+echo * sif filename is null ,default use current directory 
+echo.
+ 
+copy first\1.html 
+copy second\2.html
+
+echo. &pause
+exit
+ 
+:error
+echo. &pause
---
+++ src/changeFileName-JH-2.bat
@@ -0,0 +1,27 @@
+﻿@echo off     
+echo       *******File Change Name*******   
+echo ------------------------------------ 
+echo         Version 0.0.1      
+echo                           
+echo             Change 2015-12-28    
+echo ------------------------------------
+ 
+echo. 
+echo * please input new filename and filetype
+echo * sif filename is null ,default use current directory 
+echo.
+ 
+del 1.html 2.html
+
+
+copy thrid\3.html 3-1.html
+copy thrid\3.html 3-2.html
+
+ren 3-1.html 1.html
+ren 3-2.html 2.html
+
+echo. &pause
+exit
+ 
+:error
+echo. &pause
